1b1ff6d1c2af76a34741a1927e6bba9020282e7f
lluanps <lluanps@gmail.com>
2023-06-03 22:41:53 -0300

    ServerRepository created

---
+++ serve-management/src/main/java/com/luan/servemanagement/repository/ServerRepository.java
@@ -0,0 +1,11 @@
+package com.luan.servemanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luan.servemanagement.model.Server;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+	
+	Server findByIpAddress(String ipAddres);
+
+}
