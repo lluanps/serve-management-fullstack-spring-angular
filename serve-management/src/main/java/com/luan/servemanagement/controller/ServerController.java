4b7795d9acff52e547dd9168c29336fa8213c48e
lluanps <lluanps@gmail.com>
2023-06-04 03:41:33 -0300

    method getServer and deleteServer created

--- serve-management/src/main/java/com/luan/servemanagement/controller/ServerController.java
+++ serve-management/src/main/java/com/luan/servemanagement/controller/ServerController.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
 
-import org.springframework.data.repository.query.Param;
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,6 +55,45 @@ public class ServerController {
 					.statusCode(HttpStatus.OK.value())
 					.build()
 		);
-
 	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+		return ResponseEntity.ok(
+				Response.builder()
+					.timeStamp(LocalDate.now())
+					.data(Map.of("server", serverServiceImplementation.create(server)))
+					.message("Server created")
+					.status(HttpStatus.CREATED)
+					.statusCode(HttpStatus.CREATED.value())
+					.build()
+		);
+	}
+	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(
+				Response.builder()
+					.timeStamp(LocalDate.now())
+					.data(Map.of("server", serverServiceImplementation.get(id)))
+					.message("Server retrieved")
+					.status(HttpStatus.OK)
+					.statusCode(HttpStatus.OK.value())
+					.build()
+		);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(
+				Response.builder()
+					.timeStamp(LocalDate.now())
+					.data(Map.of("Deleted", serverServiceImplementation.delete(id)))
+					.message("Server deleted")
+					.status(HttpStatus.OK)
+					.statusCode(HttpStatus.OK.value())
+					.build()
+		);
+	}
+	
 }
