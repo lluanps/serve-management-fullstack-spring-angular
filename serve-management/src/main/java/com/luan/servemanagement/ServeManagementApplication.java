66732664d5ffe25355ce14b1b02760042def7a84
lluanps <lluanps@gmail.com>
2023-06-03 22:29:15 -0300

    Project API created

---
+++ serve-management/src/main/java/com/luan/servemanagement/ServeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.luan.servemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeManagementApplication.class, args);
+	}
+
+}
---
+++ serve-management/src/test/java/com/luan/servemanagement/ServeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.servemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
