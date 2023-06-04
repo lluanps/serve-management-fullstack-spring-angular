db7eb29c3858afb9ea0eb5ebe17d08bda08dc3bc
lluanps <lluanps@gmail.com>
2023-06-04 03:12:55 -0300

    get pingServer method created

--- serve-management/src/main/java/com/luan/servemanagement/controller/ServerController.java
+++ serve-management/src/main/java/com/luan/servemanagement/controller/ServerController.java
@@ -1,17 +1,21 @@
 package com.luan.servemanagement.controller;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luan.servemanagement.model.Response;
-import com.luan.servemanagement.service.ServerService;
+import com.luan.servemanagement.model.Server;
 import com.luan.servemanagement.service.implementation.ServerServiceImplementation;
+import com.luan.servemanagement.status.Status;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,8 +36,21 @@ public class ServerController {
 					.status(HttpStatus.OK)
 					.statusCode(HttpStatus.OK.value())
 					.build()
-				);
-				
+		);		
 	}
+	
+	@GetMapping("/ping/{ipAddress}")
+	public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+		Server server = serverServiceImplementation.ping(ipAddress);
+		return ResponseEntity.ok(
+				Response.builder()
+					.timeStamp(LocalDate.now())
+					.data(Map.of("server", server))
+					.message(server.getStatus() == Status.SERVER_UP ? "Ping success!" : "Ping failed!")
+					.status(HttpStatus.OK)
+					.statusCode(HttpStatus.OK.value())
+					.build()
+		);
 
+	}
 }
